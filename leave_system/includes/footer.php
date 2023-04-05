896005e9ab2c633102910cb01be0be5615c1b30c
DianaMuthoni <60980087+DianaMuthoni@users.noreply.github.com>
2023-04-05 12:42:33 +0300

    Add files via upload

---
+++ leave_system/includes/footer.php
@@ -0,0 +1,7 @@
+<!-- footer copyright that directs users to my portfolio -->
+
+<footer>
+        <div class="footer-area">
+                <p>© <?php echo date("Y");?> | Employee Performance Management System in PHP | Developed By <a href="https://codeastro.com">Diana, Thee Hacker</a></p>
+        </div>
+</footer>
---
+++ leave_system/login1.js
@@ -0,0 +1,9 @@
+ 
+// setting onclick events for activities, emotions, and leaves
+// also directing these evtns to leave system, index.php
+
+var mood = document.getElementById('mood')
+ mood.onclick = function() {
+     window.location.replace("http://index.php/");
+ }
+     
